e7aaa6209e4d1dda4056f191a9e35fd2d53cf84b
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2021-12-25 16:24:18 +0200

    Make the tests data oriented

--- src/tests/instructiontest.hpp
+++ src/tests/instructiontest.hpp
@@ -1,23 +1,26 @@
 #pragma once
 
-#include <gtest/gtest.h>
 #include "testutils.hpp"
+
+#include <gtest/gtest.h>
+#include <zasm/zasm.hpp>
 
 namespace zasm::tests
 {
 
+    using CodeGenFn = zasm::Error (*)(class Assembler& a);
+
+    struct InstrTest
+    {
+        const char* instrBytes;
+        const char* operation;
+        CodeGenFn gen;
+    };
+
 #define INSTRUCTION_TEST(INSTR_BYTES, ...)                                                                                     \
-    TEST(InstructionTests, T##INSTR_BYTES)                                                                                     \
+    InstrTest                                                                                                                  \
     {                                                                                                                          \
-        using namespace zasm::operands;                                                                                        \
-                                                                                                                               \
-        Program program(ZYDIS_MACHINE_MODE_LONG_64);                                                                           \
-        Assembler assembler(program);                                                                                          \
-                                                                                                                               \
-        ASSERT_EQ(assembler.__VA_ARGS__, Error::None);                                                                         \
-        ASSERT_EQ(program.serialize(0x0000000000401000), Error::None);                                                         \
-        auto hexEncoded = hexEncode(program.getCode(), program.getCodeSize());                                                 \
-        ASSERT_EQ(std::string(#INSTR_BYTES), hexEncoded) << #__VA_ARGS__;                                                      \
+#INSTR_BYTES, #__VA_ARGS__, [](auto& a) { using namespace zasm::operands; return a.__VA_ARGS__; }                              \
     }
 
 } // namespace zasm::tests
