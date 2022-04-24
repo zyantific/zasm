1001639d2d23c701eee2e0b96bf302c215c07ea5
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2022-04-24 17:33:41 +0300

    Add more benchmarks

---
+++ src/benchmark/benchmarks/benchmark.stringpool.cpp
@@ -0,0 +1,79 @@
+#include <benchmark/benchmark.h>
+#include <zasm/core/stringpool.hpp>
+
+namespace zasm::benchmarks
+{
+    static const std::string TestStrings[] =
+    {
+        "hello",
+        "world",
+        "longer string",
+        "even longer string",
+        "even longer longer string"
+    };
+
+    static void BM_StringPool_Aquire(benchmark::State& state)
+    {
+        StringPool pool;
+        for (auto _ : state)
+        {
+            const auto& str = TestStrings[state.range(0)];
+
+            auto stringId = pool.aquire(str);
+            benchmark::DoNotOptimize(stringId);
+        }
+    }
+    BENCHMARK(BM_StringPool_Aquire)->DenseRange(0, std::size(TestStrings) - 1);
+
+    static void BM_StringPool_Release(benchmark::State& state)
+    {
+        StringPool pool;
+        for (auto _ : state)
+        {
+            state.PauseTiming();
+            const auto& str = TestStrings[state.range(0)];
+
+            auto stringId = pool.aquire(str);
+            state.ResumeTiming();
+
+            auto refCount = pool.release(stringId);
+            benchmark::DoNotOptimize(refCount);
+        }
+    }
+    BENCHMARK(BM_StringPool_Release)->DenseRange(0, std::size(TestStrings) - 1);
+
+    static void BM_StringPool_Get(benchmark::State& state)
+    {
+        StringPool pool;
+        for (auto _ : state)
+        {
+            state.PauseTiming();
+            const auto& str = TestStrings[state.range(0)];
+
+            auto stringId = pool.aquire(str);
+            state.ResumeTiming();
+
+            const char* res = pool.get(stringId);
+            benchmark::DoNotOptimize(res);
+        }
+    }
+    BENCHMARK(BM_StringPool_Get)->DenseRange(0, std::size(TestStrings) - 1);
+
+    static void BM_StringPool_GetLength(benchmark::State& state)
+    {
+        StringPool pool;
+        for (auto _ : state)
+        {
+            state.PauseTiming();
+            const auto& str = TestStrings[state.range(0)];
+
+            auto stringId = pool.aquire(str);
+            state.ResumeTiming();
+
+            auto strLen = pool.getLength(stringId);
+            benchmark::DoNotOptimize(strLen);
+        }
+    }
+    BENCHMARK(BM_StringPool_GetLength)->DenseRange(0, std::size(TestStrings) - 1);
+   
+} // namespace zasm::benchmarks
