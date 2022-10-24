79e6bacba3e14f9b843dc911e280a684485351e0
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2022-10-25 01:10:32 +0300

    Add regression test for #74

--- src/tests/tests/tests.decoder.cpp
+++ src/tests/tests/tests.decoder.cpp
@@ -5,7 +5,7 @@
 
 namespace zasm::tests
 {
-    TEST(DecoderTests, DecodeToAssembler)
+    TEST(DecoderTests, DecodeToAssembler01)
     {
         Program program(MachineMode::AMD64);
         Decoder decoder(MachineMode::AMD64);
@@ -18,10 +18,10 @@ namespace zasm::tests
 
         auto decoded = decoder.decode(inputBytes.data(), inputBytes.size(), 0x00400000);
         ASSERT_TRUE(decoded);
-        
+
         ASSERT_EQ(decoded->getMnemonic(), x86::Mnemonic::Mov);
         ASSERT_EQ(decoded->getOperandCount(), 2);
-        
+
         ASSERT_EQ(assembler.emit(decoded.value()), Error::None);
 
         Serializer serializer;
@@ -31,7 +31,45 @@ namespace zasm::tests
             0xB8, 0x01, 0x00, 0x00, 0x00,
         };
         ASSERT_EQ(serializer.getCodeSize(), expected.size());
-        
+
+        const auto* data = serializer.getCode();
+        ASSERT_NE(data, nullptr);
+        for (size_t i = 0; i < expected.size(); i++)
+        {
+            ASSERT_EQ(data[i], expected[i]);
+        }
+    }
+
+    TEST(DecoderTests, Regression_74)
+    {
+        Program program(MachineMode::AMD64);
+        Decoder decoder(MachineMode::AMD64);
+
+        x86::Assembler assembler(program);
+
+        const std::array<uint8_t, 6> inputBytes = {
+            0xFF, 0x15, 0x73, 0x16, 0x00, 0x00, // CALL QWORD PTR DS:[0x00007FF8833E0679]
+        };
+
+        auto decoded = decoder.decode(inputBytes.data(), inputBytes.size(), 0x00007FF8833DF000);
+        ASSERT_TRUE(decoded);
+
+        ASSERT_EQ(decoded->getMnemonic(), x86::Mnemonic::Call);
+        ASSERT_EQ(decoded->getExplicitOperandCount(), 1);
+        ASSERT_EQ(decoded->getOperandCount(), 4);
+        ASSERT_EQ(decoded->getOperand<Mem>(0).getBase(), x86::rip);
+        ASSERT_EQ(decoded->getOperand<Mem>(0).getDisplacement(), 0x00007FF8833E0679);
+
+        ASSERT_EQ(assembler.emit(decoded.value()), Error::None);
+
+        Serializer serializer;
+        ASSERT_EQ(serializer.serialize(program, 0x00007FF8833DF000), Error::None);
+
+        const std::array<uint8_t, 6> expected = {
+            0xFF, 0x15, 0x73, 0x16, 0x00, 0x00,
+        };
+        ASSERT_EQ(serializer.getCodeSize(), expected.size());
+
         const auto* data = serializer.getCode();
         ASSERT_NE(data, nullptr);
         for (size_t i = 0; i < expected.size(); i++)
