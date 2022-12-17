5cd8afed6ed404bab6e768549767f6c4b72b0a9b
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2022-12-17 17:21:38 +0200

    Make CPUFlags a strong type and include in instruction info example

--- examples/instruction_info/main.cpp
+++ examples/instruction_info/main.cpp
@@ -1,6 +1,44 @@
 #include "examples.common.hpp"
 
 #include <iostream>
+#include <zasm/x86/meta.hpp>
+
+static std::string getCPUFlagsString(zasm::InstrCPUFlags flags)
+{
+    namespace CPUFlags = zasm::x86::CPUFlags;
+
+    std::string str;
+
+    const auto appendFlag = [&](auto fl, const auto& name) {
+        if ((flags & fl) != CPUFlags::None)
+        {
+            if (!str.empty())
+                str += " | ";
+            str += name;
+        }
+    };
+
+    appendFlag(CPUFlags::CF, "CF");
+    appendFlag(CPUFlags::PF, "PF");
+    appendFlag(CPUFlags::AF, "AF");
+    appendFlag(CPUFlags::ZF, "ZF");
+    appendFlag(CPUFlags::SF, "SF");
+    appendFlag(CPUFlags::TF, "TF");
+    appendFlag(CPUFlags::IF, "IF");
+    appendFlag(CPUFlags::DF, "DF");
+    appendFlag(CPUFlags::OF, "OF");
+    appendFlag(CPUFlags::IOPL1, "IOPL1");
+    appendFlag(CPUFlags::IOPL2, "IOPL2");
+    appendFlag(CPUFlags::NT, "NT");
+    appendFlag(CPUFlags::RF, "RF");
+    appendFlag(CPUFlags::VM, "VM");
+    appendFlag(CPUFlags::AC, "AC");
+    appendFlag(CPUFlags::VIF, "VIF");
+    appendFlag(CPUFlags::VIP, "VIP");
+    appendFlag(CPUFlags::ID, "ID");
+
+    return str;
+}
 
 static void printInstructionInfo(zasm::MachineMode mode, const zasm::Instruction& instr)
 {
@@ -13,21 +51,38 @@ static void printInstructionInfo(zasm::MachineMode mode, const zasm::Instruction
         return;
     }
 
-    std::cout << "Instruction: " << formatter::toString(&instr) << "\n";
+    std::cout << "Instruction: " << formatter::toString(&instr) << " {\n";
+
+    const auto& cpuFlags = info->getCPUFlags();
+    std::cout << "  CPUFlags = {\n";
+    if (cpuFlags.set0 != x86::CPUFlags::None)
+        std::cout << "    Set 0 = " << getCPUFlagsString(cpuFlags.set0) << ",\n";
+    if (cpuFlags.set1 != x86::CPUFlags::None)
+        std::cout << "    Set 1 = " << getCPUFlagsString(cpuFlags.set1) << ",\n";
+    if (cpuFlags.modified != x86::CPUFlags::None)
+        std::cout << "    Modified = " << getCPUFlagsString(cpuFlags.modified) << ",\n";
+    if (cpuFlags.tested != x86::CPUFlags::None)
+        std::cout << "    Tested = " << getCPUFlagsString(cpuFlags.tested) << ",\n";
+    if (cpuFlags.undefined != x86::CPUFlags::None)
+        std::cout << "    Undefined = " << getCPUFlagsString(cpuFlags.undefined) << ",\n";
+    std::cout << "  },\n";
+
     for (size_t i = 0; i < info->getOperandCount(); ++i)
     {
         const Operand& op = info->getOperand(i);
 
         std::cout << "  Operand[" << i << "] = {\n";
-
+        
+        std::cout << "    Size = " << getBitSize(op.getBitSize(mode)) << "b,\n";
+        
         if (auto* opMem = op.getIf<Mem>(); opMem != nullptr)
         {
             std::cout << "    Type = Mem(\n";
-            std::cout << "      Segment: " << formatter::toString(opMem->getSegment()) << "\n";
-            std::cout << "      Base: " << formatter::toString(opMem->getBase()) << "\n";
+            std::cout << "      Seg:   " << formatter::toString(opMem->getSegment()) << "\n";
+            std::cout << "      Base:  " << formatter::toString(opMem->getBase()) << "\n";
             std::cout << "      Index: " << formatter::toString(opMem->getIndex()) << "\n";
             std::cout << "      Scale: " << (int)opMem->getScale() << "\n";
-            std::cout << "      Displacement: " << opMem->getDisplacement() << "\n";
+            std::cout << "      Disp:  " << opMem->getDisplacement() << "\n";
             std::cout << "    ),\n";
         }
         else if (auto* opReg = op.getIf<Reg>(); opReg != nullptr)
@@ -65,7 +120,7 @@ static void printInstructionInfo(zasm::MachineMode mode, const zasm::Instruction
             else if ((opAccess & Operand::Access::Write) != Operand::Access::None)
                 std::cout << "Write ";
 
-            std::cout << "\n";
+            std::cout << ",\n";
         }
 
         std::cout << "    Visibility = ";
@@ -85,17 +140,24 @@ static void printInstructionInfo(zasm::MachineMode mode, const zasm::Instruction
                 std::cout << "Hidden";
                 break;
         }
-        std::cout << "\n";
+        std::cout << ",\n";
 
-        std::cout << "    Size = " << getBitSize(op.getBitSize(mode)) << "b\n";
-
-        std::cout << "  }\n";
+        std::cout << "  },\n";
     }
+    std::cout << "}\n";
 }
 
 int main()
 {
     using namespace zasm;
+
+    {
+        const auto instr = Instruction()                        //
+                               .setMnemonic(x86::Mnemonic::Sub) //
+                               .addOperand(x86::rax)            //
+                               .addOperand(Imm(1));             //
+        printInstructionInfo(MachineMode::AMD64, instr);
+    }
 
     {
         const auto instr = Instruction()                        //
