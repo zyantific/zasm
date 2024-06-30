e6b521b670d0262bf6a77a9f90edcf3d1ffb0777
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2024-06-30 14:19:48 +0300

    #134: Implement remaining enum operators, add tests

--- zasm/include/zasm/core/enumflags.hpp
+++ zasm/include/zasm/core/enumflags.hpp
@@ -11,18 +11,33 @@ namespace zasm
     /// </summary>
     /// <typeparam name="ENM">Enum type</typeparam>
 #define ZASM_ENABLE_ENUM_OPERATORS(ENM)                                                                                        \
-    static_assert(std::is_unsigned_v<std::underlying_type_t<ENM>> == true, "Underlying enum type must be unsignd");            \
+    static_assert(std::is_unsigned_v<std::underlying_type_t<ENM>> == true, "Underlying enum type must be unsigned");           \
     inline constexpr ENM operator|(const ENM a, const ENM b) noexcept                                                          \
     {                                                                                                                          \
         return static_cast<ENM>(static_cast<std::underlying_type_t<ENM>>(a) | static_cast<std::underlying_type_t<ENM>>(b));    \
+    }                                                                                                                          \
+    inline constexpr ENM operator|=(ENM& a, const ENM b) noexcept                                                              \
+    {                                                                                                                          \
+        a = static_cast<ENM>(static_cast<std::underlying_type_t<ENM>>(a) | static_cast<std::underlying_type_t<ENM>>(b));       \
+        return a;                                                                                                              \
     }                                                                                                                          \
     inline constexpr ENM operator^(const ENM a, const ENM b) noexcept                                                          \
     {                                                                                                                          \
         return static_cast<ENM>(static_cast<std::underlying_type_t<ENM>>(a) ^ static_cast<std::underlying_type_t<ENM>>(b));    \
     }                                                                                                                          \
+    inline constexpr ENM operator^=(ENM& a, const ENM b) noexcept                                                              \
+    {                                                                                                                          \
+        a = static_cast<ENM>(static_cast<std::underlying_type_t<ENM>>(a) ^ static_cast<std::underlying_type_t<ENM>>(b));       \
+        return a;                                                                                                              \
+    }                                                                                                                          \
     inline constexpr ENM operator&(const ENM a, const ENM b) noexcept                                                          \
     {                                                                                                                          \
         return static_cast<ENM>(static_cast<std::underlying_type_t<ENM>>(a) & static_cast<std::underlying_type_t<ENM>>(b));    \
+    }                                                                                                                          \
+    inline constexpr ENM operator&=(ENM& a, const ENM b) noexcept                                                              \
+    {                                                                                                                          \
+        a = static_cast<ENM>(static_cast<std::underlying_type_t<ENM>>(a) & static_cast<std::underlying_type_t<ENM>>(b));       \
+        return a;                                                                                                              \
     }                                                                                                                          \
     inline constexpr ENM operator~(const ENM a) noexcept                                                                       \
     {                                                                                                                          \
