bc430533b3696cd1071b9ef61dc8be4255347fec
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2024-07-02 16:52:42 +0300

    Add isOperandType, improve documentation

--- zasm/include/zasm/base/instruction.hpp
+++ zasm/include/zasm/base/instruction.hpp
@@ -97,7 +97,7 @@ namespace zasm
         /// ex.: x86::Attribs::Lock
         /// </summary>
         /// <param name="attrib">Attributes to add</param>
-        /// <returns>Instruction&</returns>
+        /// <returns>*this</returns>
         constexpr TBase& addAttribs(Attribs attrib)
         {
             _attribs = _attribs | attrib;
@@ -109,13 +109,18 @@ namespace zasm
         /// ex.: x86::Attribs::Lock
         /// </summary>
         /// <param name="attrib">Attributes to remove</param>
-        /// <returns>Instruction&</returns>
+        /// <returns>*this</returns>
         constexpr TBase& removeAttribs(Attribs attrib)
         {
             _attribs = _attribs & ~attrib;
             return static_cast<TBase&>(*this);
         }
 
+        /// <summary>
+        /// Adds a new operand to the instruction increasing the operand count by one.
+        /// </summary>
+        /// <param name="val">New operand</param>
+        /// <returns>*this</returns>
         constexpr TBase& addOperand(const Operand& val) noexcept
         {
             assert(_opCount < _operands.size());
@@ -127,6 +132,12 @@ namespace zasm
             return static_cast<TBase&>(*this);
         }
 
+        /// <summary>
+        /// Sets the operand at the specified index, an already existing operand must exist at the index otherwise its a no-op.
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="val"></param>
+        /// <returns>*this</returns>
         constexpr TBase& setOperand(std::size_t index, const Operand& val) noexcept
         {
             assert(index < _opCount);
@@ -138,21 +149,39 @@ namespace zasm
             return static_cast<TBase&>(*this);
         }
 
+        /// <summary>
+        /// Returns the array to the operands storage, use getOperandCount to get the amount of used operands.
+        /// </summary>
+        /// <returns>Reference to operands</returns>
         constexpr const Operands& getOperands() const noexcept
         {
             return _operands;
         }
 
+        /// <summary>
+        /// Returns the array to the operands storage, use getOperandCount to get the amount of used operands.
+        /// </summary>
+        /// <returns>Reference to operands</returns>
         constexpr Operands& getOperands() noexcept
         {
             return _operands;
         }
 
+        /// <summary>
+        /// Returns the amount of used operands.
+        /// </summary>
+        /// <returns>Operand count</returns>
         constexpr std::size_t getOperandCount() const noexcept
         {
             return _opCount;
         }
 
+        /// <summary>
+        /// Returns the a reference to the operand at the specified compile time index if it is of the specified type T,
+        /// </summary>
+        /// <typeparam name="T">Expected operand type</typeparam>
+        /// <typeparam name="TIndex">Operand index</typeparam>
+        /// <returns>Reference to operand as T</returns>
         template<std::size_t TIndex, typename T = Operand> constexpr const T& getOperand() const
         {
             if constexpr (std::is_same_v<T, Operand>)
@@ -166,6 +195,12 @@ namespace zasm
             }
         }
 
+        /// <summary>
+        /// Returns the a reference to the operand at the specified compile time index if it is of the specified type T,
+        /// </summary>
+        /// <typeparam name="T">Expected operand type</typeparam>
+        /// <typeparam name="TIndex">Operand index</typeparam>
+        /// <returns>Reference to operand as T</returns>
         template<std::size_t TIndex, typename T = Operand> constexpr T& getOperand()
         {
             if constexpr (std::is_same_v<T, Operand>)
@@ -179,6 +214,13 @@ namespace zasm
             }
         }
 
+        /// <summary>
+        /// Returns the a reference to the operand at the specified index if it is of the specified type T,
+        /// will throw if the type does not match.
+        /// </summary>
+        /// <typeparam name="T">Expected operand type</typeparam>
+        /// <param name="index">Operand index</param>
+        /// <returns>A reference to the operand as T</returns>
         template<typename T = Operand> constexpr const T& getOperand(std::size_t index) const
         {
             if constexpr (std::is_same_v<T, Operand>)
@@ -192,6 +234,13 @@ namespace zasm
             }
         }
 
+        /// <summary>
+        /// Returns the a reference to the operand at the specified index if it is of the specified type T,
+        /// will throw if the type does not match.
+        /// </summary>
+        /// <typeparam name="T">Expected operand type</typeparam>
+        /// <param name="index">Operand index</param>
+        /// <returns>A reference to the operand as T</returns>
         template<typename T = Operand> constexpr T& getOperand(std::size_t index)
         {
             if constexpr (std::is_same_v<T, Operand>)
@@ -205,6 +254,12 @@ namespace zasm
             }
         }
 
+        /// <summary>
+        /// Returns the operand at the specified index if it is of the specified type T, otherwise nullptr.
+        /// </summary>
+        /// <typeparam name="T">Expected operand type</typeparam>
+        /// <param name="index">Operand index</param>
+        /// <returns>The operand as T if the type matches, otherwise nullptr</returns>
         template<typename T> constexpr const T* getOperandIf(std::size_t index) const noexcept
         {
             if (index >= _opCount || index >= _operands.size())
@@ -216,6 +271,12 @@ namespace zasm
             return operand.template getIf<T>();
         }
 
+        /// <summary>
+        /// Returns the operand at the specified index if it is of the specified type T, otherwise nullptr.
+        /// </summary>
+        /// <typeparam name="T">Expected operand type</typeparam>
+        /// <param name="index">Operand index</param>
+        /// <returns>The operand as T if the type matches, otherwise nullptr</returns>
         template<typename T> constexpr T* getOperandIf(std::size_t index) noexcept
         {
             if (index >= _opCount || index >= _operands.size())
@@ -224,6 +285,22 @@ namespace zasm
             }
             auto& operand = _operands[index];
             return operand.template getIf<T>();
+        }
+
+        /// <summary>
+        /// Checks if the operand at the specified index is of the specified type T.
+        /// </summary>
+        /// <typeparam name="T">The type to check</typeparam>
+        /// <param name="index">Operand index</param>
+        /// <returns>True if the operand is type T</returns>
+        template<typename T> constexpr bool isOperandType(std::size_t index) const noexcept
+        {
+            if (index >= _opCount || index >= _operands.size())
+            {
+                return false;
+            }
+            const auto& operand = _operands[index];
+            return operand.template holds<T>();
         }
     };
 
