439a99a800f1473c464ef45b609a1cd1ddb9961f
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2024-09-20 08:55:10 +0300

    Adjust decode to assembler example

--- examples/decode_to_assembler/main.cpp
+++ examples/decode_to_assembler/main.cpp
@@ -7,7 +7,15 @@ int main()
     using namespace zasm;
 
     const uint64_t baseAddr = 0x00007FF6BC738ED4;
-    const std::array<uint8_t, 24> code = {
+    const std::array<uint8_t, 48> code = {
+        0x40, 0x53,             // push rbx
+        0x45, 0x8B, 0x18,       // mov r11d, dword ptr ds:[r8]
+        0x48, 0x8B, 0xDA,       // mov rbx, rdx
+        0x41, 0x83, 0xE3, 0xF8, // and r11d, 0xFFFFFFF8
+        0x4C, 0x8B, 0xC9,       // mov r9, rcx
+        0x41, 0xF6, 0x00, 0x04, // test byte ptr ds:[r8], 0x4
+        0x4C, 0x8B, 0xD1,       // mov r10, rcx
+        0x74, 0x13,             // je 0x00007FF6BC738EFF
         0x40, 0x53,             // push rbx
         0x45, 0x8B, 0x18,       // mov r11d, dword ptr ds:[r8]
         0x48, 0x8B, 0xDA,       // mov rbx, rdx
@@ -37,15 +45,19 @@ int main()
             return EXIT_FAILURE;
         }
 
-        const auto& instrInfo = *decoderRes;
-        
-        const auto instr = instrInfo.getInstruction();
+        const auto& instr = decoderRes.value();
         if (auto res = assembler.emit(instr); res != zasm::ErrorCode::None)
         {
             std::cout << "Failed to emit instruction " << std::hex << curAddress << ", " << res.getErrorName() << "\n";
         }
 
-        bytesDecoded += instrInfo.getLength();
+        bytesDecoded += instr.getLength();
+
+        if (x86::isBranching(instr))
+        {
+            // Decode only a single branch.
+            break;
+        }
     }
 
     Serializer serializer;
